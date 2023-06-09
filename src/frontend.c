dbf1bea959307713750e24e5b4581243da893455
ZmeetL <1113989231@qq.com>
2023-06-09 11:13:36 +0800

    feat: flex print

--- src/frontend.c
+++ src/frontend.c
@@ -4,6 +4,7 @@
 #include <frontend/parser.h>
 
 p_ast_program frontend_trans(const char *file_name) {
+    printf("=== lexical analysis begin ===\n");
     yyscan_t scanner;
     extra_info extra = (extra_info) {
         .fs = NULL,
@@ -16,5 +17,6 @@ p_ast_program frontend_trans(const char *file_name) {
     p_ast_program p_ast = yyget_extra(scanner)->p_ast;
 
     yylex_destroy(scanner);
+    printf("=== lexical analysis end ===\n");
     return p_ast;
 }
